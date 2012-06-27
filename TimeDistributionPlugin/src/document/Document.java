b423b8a229b59600f7fcc9ff4a437ef91c28db37
Wouter Willems <wouterwillems10@gmail.com>
2012-06-27 17:23:57 +0200

    intervalToXml stub

--- TimeDistributionPlugin/src/document/Document.java
+++ TimeDistributionPlugin/src/document/Document.java
@@ -4,8 +4,18 @@ public class Document implements IDocument {
 	private String fileName;
 	private DocumentType docType;	
 	
+	public Document(){}
+	
 	public Document(String fileName, DocumentType docType){
 		this.fileName = fileName;
+		this.docType = docType;
+	}
+	
+	public void setFileName(String name){
+		this.fileName = name;
+	}
+	
+	public void setDocType(DocumentType docType){
 		this.docType = docType;
 	}
 	
---
+++ TimeDistributionPlugin/test/timingOutput/IntervalsToXMLWriterTest.java
@@ -0,0 +1,14 @@
+package timingOutput;
+
+import interval.IInterval;
+
+import org.junit.Test;
+
+public class IntervalsToXMLWriterTest {
+	
+	@Test
+	public void intervalToXMLTest(IInterval interval){
+		
+	}
+	
+}
