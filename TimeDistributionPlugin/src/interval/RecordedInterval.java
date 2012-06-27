263f1059d63dbac64af0c734a50c0a826d88980b
Wouter Willems <wouterwillems10@gmail.com>
2012-06-27 15:14:39 +0200

    joda time

--- TimeDistributionPlugin/src/interval/RecordedInterval.java
+++ TimeDistributionPlugin/src/interval/RecordedInterval.java
@@ -2,9 +2,11 @@ package interval;
 
 import java.util.Date;
 
+import org.joda.time.Duration;
+
 import document.IDocument;
 
-public class RecordedInterval extends Interval {
+public class RecordedInterval implements IInterval {
 
 	private IDocument document;
 	private Date start;
@@ -16,16 +18,24 @@ public class RecordedInterval extends Interval {
 		this.end = end;
 	}
 
+	@Override
 	public IDocument getDocument() {
 		return document;
 	}
 	
+	@Override
 	public Date getStart(){
 		return start;
 	}
 	
+	@Override
 	public Date getEnd(){
 		return end;
+	}
+	
+	@Override 
+	public Duration getDuration(){
+		return new Duration(start.getTime(), end.getTime());
 	}
 
 }
