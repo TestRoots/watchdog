c16182767ba565ed4cb7ed3ec91e6130d1210d59
Wouter Willems <wouterwillems10@gmail.com>
2012-07-10 14:28:25 +0200

    file dialog for saving xml exports

--- TimeDistributionPlugin/src/nl/tudelft/watchdog/timeDistributionPlugin/commands/ExportHandler.java
+++ TimeDistributionPlugin/src/nl/tudelft/watchdog/timeDistributionPlugin/commands/ExportHandler.java
@@ -15,6 +15,10 @@ import nl.tudelft.watchdog.timingOutput.IntervalsToXMLWriter;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.console.MessageConsoleStream;
 
 
@@ -30,21 +34,37 @@ public class ExportHandler extends AbstractHandler{
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {		
-		stream.println("Wroof!");
-		
+		stream.println("Wroof!");		
 		
 		IIntervalKeeper intervalKeeper = IntervalKeeper.getInstance();
 		for(IInterval interval : intervalKeeper.getRecordedIntervals()){
 			 stream.println(interval.getDocument().getFileName() +"\t\t" + interval.getDurationString()+ "\t\t" + interval.getStart()+" - "+interval.getEnd());			 
 		}
-		try{
-			File f = new File("watchdog/intervals.xml");
-			f.mkdirs();
-			FileOutputStream fos = new FileOutputStream(f);
+		saveToFile();
+		return null;
+	}
+	
+	private void saveToFile(){
+		Display display = Display.getCurrent();
+	    Shell shell = new Shell(display);
+	    FileDialog dialog = new FileDialog(shell, SWT.SAVE);
+	    dialog.setFilterNames(new String[] { "XML files", "All Files (*.*)" });
+	    dialog.setFilterExtensions(new String[] { "*.xml", "*.*" }); // Windows
+	                                    // wild
+	                                    // cards
+	    dialog.setFilterPath("c:\\"); // Windows path
+	    dialog.setFileName("WatchDogIntervals.xml");
+	    
+	    String path = dialog.open();
+	    
+	    File f = new File(path);
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(f);
 			intervalWriter.exportIntervals(IntervalKeeper.getInstance().getRecordedIntervals(), fos);
 		} catch (FileNotFoundException e) {
+			//TODO: some error handling here
 			MyLogger.logSevere(e);
 		}
-		return null;
 	}
 }   
