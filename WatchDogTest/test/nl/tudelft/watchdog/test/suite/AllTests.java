d1e7f87b8edce3e987e8cd93e5d9465e50842572
Wouter Willems <wouterwillems10@gmail.com>
2012-08-02 16:32:35 +0200

    interval testing removed, hard to test with timeouts

--- WatchDogTest/test/nl/tudelft/watchdog/test/suite/AllTests.java
+++ WatchDogTest/test/nl/tudelft/watchdog/test/suite/AllTests.java
@@ -3,7 +3,6 @@ package nl.tudelft.watchdog.test.suite;
 
 import nl.tudelft.watchdog.test.document.DocumentClassifierTest;
 import nl.tudelft.watchdog.test.document.DocumentFactoryTest;
-import nl.tudelft.watchdog.test.interval.ActiveEditingIntervalTest;
 import nl.tudelft.watchdog.test.timingOutput.IntervalsToXMLWriterTest;
 
 import org.junit.runner.RunWith;
@@ -12,7 +11,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({ActiveEditingIntervalTest.class, IntervalsToXMLWriterTest.class, DocumentClassifierTest.class, DocumentFactoryTest.class})
+@SuiteClasses({IntervalsToXMLWriterTest.class, DocumentClassifierTest.class, DocumentFactoryTest.class})
 public class AllTests {
 	
 }
