ac4b1db0a5e628f6ca195747c3a5facfd4e378e8
nightraven <moritzbeller@gmx.de>
2014-02-20 17:50:01 +0100

    - Makes test pass - Removes deprecated Junit usage - Adds some JavaDoc
    Closes #7

--- WatchDogTest/test/nl/tudelft/watchdog/test/document/DocumentClassifierTest.java
+++ WatchDogTest/test/nl/tudelft/watchdog/test/document/DocumentClassifierTest.java
@@ -1,59 +1,51 @@
 package nl.tudelft.watchdog.test.document;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
 import nl.tudelft.watchdog.document.DocumentClassifier;
 import nl.tudelft.watchdog.document.DocumentType;
 
 import org.junit.Test;
 
+/**
+ * Tests the recognition of a file as a production, a development or an
+ * undefined file, as done by the {@link DocumentClassifier}.
+ */
 public class DocumentClassifierTest {
 
 	@Test
 	public void testProductionDocumentClassification() {
-		String contents = 
-				"public class ProductionClass{" +
-				"	public ProductionClass{" +
-				"		//do something" +
-				"	}" +
-				"}";
-		DocumentType type = DocumentClassifier.classifyDocument("ProductionClass.java", contents);
-		Assert.assertEquals(DocumentType.PRODUCTION, type);
+		String contents = "public class ProductionClass{"
+				+ "	public ProductionClass{" + "		//do something" + "	}" + "}";
+		DocumentType type = DocumentClassifier.classifyDocument(
+				"ProductionClass.java", contents);
+		assertEquals(DocumentType.PRODUCTION, type);
 	}
-	
+
 	@Test
 	public void testTestDocumentClassification() {
-		String contents = 
-				"import org.junit.assert" +
-				"public class TestClass{" +
-				"	@Test" +
-				"	public testSomething{" +
-				"		//do something" +
-				"	}" +
-				"}";
-		DocumentType type = DocumentClassifier.classifyDocument("TestClass.java", contents);
-		Assert.assertEquals(DocumentType.TEST, type);
+		String contents = "import org.junit.assert" + "public class TestClass{"
+				+ "	@Test" + "	public testSomething{" + "		//do something"
+				+ "	}" + "}";
+		DocumentType type = DocumentClassifier.classifyDocument(
+				"TestClass.java", contents);
+		assertEquals(DocumentType.TEST, type);
 	}
-	
+
 	@Test
 	public void testTestDocumentClassificationWithoutJUnitImports() {
-		String contents = 
-				"public class TestClass{" +
-				"	@Test" +
-				"	public testSomething{" +
-				"		//do something" +
-				"	}" +
-				"}";
-		DocumentType type = DocumentClassifier.classifyDocument("TestClass.java", contents);
-		Assert.assertEquals(DocumentType.PRODUCTION, type);
+		String contents = "public class TestClass{" + "	@Test"
+				+ "	public testSomething{" + "		//do something" + "	}" + "}";
+		DocumentType type = DocumentClassifier.classifyDocument(
+				"TestClass.java", contents);
+		assertEquals(DocumentType.PRODUCTION, type);
 	}
-	
+
 	@Test
 	public void testUndefinedDocumentClassification() {
-		String contents = 
-				"The quick brown fox... etc";
-		DocumentType type = DocumentClassifier.classifyDocument("somefile.txt", contents);
-		Assert.assertEquals(DocumentType.UNDEFINED, type);
+		String contents = "The quick brown fox... etc";
+		DocumentType type = DocumentClassifier.classifyDocument("somefile.txt",
+				contents);
+		assertEquals(DocumentType.UNDEFINED, type);
 	}
-	
 
 }
