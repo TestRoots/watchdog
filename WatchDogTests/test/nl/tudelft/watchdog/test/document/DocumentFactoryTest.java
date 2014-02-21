eb211538c4704baec1e65de51f1cf6607251a322
nightraven <moritzbeller@gmx.de>
2014-02-21 11:16:12 +0100

    Clean up of WatchDogTests
    Refs #6

--- WatchDogTests/test/nl/tudelft/watchdog/test/document/DocumentFactoryTest.java
+++ WatchDogTests/test/nl/tudelft/watchdog/test/document/DocumentFactoryTest.java
@@ -9,27 +9,28 @@ import nl.tudelft.watchdog.document.DocumentFactory;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.mockito.Matchers;
 
 public class DocumentFactoryTest {
 
-	@Test
-	public void testCreateDocument() {
-		String contents = "public class A {}";
-		
-		ITextEditor mockedEditor = mock(ITextEditor.class);
-		IDocumentProvider mockedProvider = mock(IDocumentProvider.class);
-		org.eclipse.jface.text.IDocument mockedDocument = mock(org.eclipse.jface.text.IDocument.class);
-		
-		when(mockedEditor.getDocumentProvider()).thenReturn(mockedProvider);
-		when(mockedProvider.getDocument(Mockito.anyObject())).thenReturn(mockedDocument);
-		when(mockedDocument.get()).thenReturn(contents);
-		
-		when(mockedEditor.getTitle()).thenReturn("A.java");
-		
-		Document doc = new DocumentFactory().createDocument(mockedEditor);
-		
-		Assert.assertEquals("A.java", doc.getFileName());
-	}
+    @Test
+    public void testCreateDocument() {
+	String contents = "public class A {}";
+
+	ITextEditor mockedEditor = mock(ITextEditor.class);
+	IDocumentProvider mockedProvider = mock(IDocumentProvider.class);
+	org.eclipse.jface.text.IDocument mockedDocument = mock(org.eclipse.jface.text.IDocument.class);
+
+	when(mockedEditor.getDocumentProvider()).thenReturn(mockedProvider);
+	when(mockedProvider.getDocument(Matchers.anyObject())).thenReturn(
+		mockedDocument);
+	when(mockedDocument.get()).thenReturn(contents);
+
+	when(mockedEditor.getTitle()).thenReturn("A.java");
+
+	Document doc = new DocumentFactory().createDocument(mockedEditor);
+
+	Assert.assertEquals("A.java", doc.getFileName());
+    }
 
 }
