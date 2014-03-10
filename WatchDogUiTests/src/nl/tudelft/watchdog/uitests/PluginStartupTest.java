c1c4e81298779f589de781b9eb314cb6c2f57157
Moritz Beller <moritzbeller@gmx.de>
2014-03-10 15:31:03 +0100

    Adds Tests for startup statistics.
    Refs #21

--- WatchDogUiTests/src/nl/tudelft/watchdog/uitests/PluginStartupTest.java
+++ WatchDogUiTests/src/nl/tudelft/watchdog/uitests/PluginStartupTest.java
@@ -1,35 +1,29 @@
 package nl.tudelft.watchdog.uitests;
 
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarButton;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 /**
  * Tests for whether the plugin starts up and initializes the workbench with the
  * correct icons and menus.
  */
-@RunWith(SWTBotJunit4ClassRunner.class)
-public class PluginStartupTest {
-
-	/** The {@link SWTWorkbenchBot} bot. */
-	private static SWTWorkbenchBot bot;
-
-	/** Dismisses the startup screen from eclipse and setups the bot. */
-	@BeforeClass
-	public static void beforeClass() throws Exception {
-		bot = new SWTWorkbenchBot();
-		bot.viewByTitle("Welcome").close();
-	}
+public class PluginStartupTest extends EclipseWorkbenchInitializer {
 
 	/** Tests whether the command menu contribution from WatchDog active. */
 	@Test
 	public void testCommandMenuContribution() {
 		SWTBotMenu fileMenu = bot.menu("WatchDog");
 		Assert.assertNotNull(fileMenu);
+	}
+
+	/** Tests whether the toolbar menu contribution from WatchDog active. */
+	@Test
+	public void testToolbarMenuContribution() {
+		SWTBotToolbarButton button = bot
+				.toolbarButtonWithTooltip("WatchDog is active and recording ...");
+		Assert.assertNotNull(button);
 	}
 
 }
