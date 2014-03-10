3be9815037cf4fa2f28648ee73983ee58c7e3e1d
Moritz Beller <moritzbeller@gmx.de>
2014-03-10 13:36:26 +0100

    Adds further refinements (javadoc)
    Refs #21

--- WatchDogUiTests/src/nl/tudelft/watchdog/uitests/PluginStartupTest.java
+++ WatchDogUiTests/src/nl/tudelft/watchdog/uitests/PluginStartupTest.java
@@ -3,33 +3,33 @@ package nl.tudelft.watchdog.uitests;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ * Tests for whether the plugin starts up and initializes the workbench with the
+ * correct icons and menus.
+ */
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class PluginStartupTest {
 
+	/** The {@link SWTWorkbenchBot} bot. */
 	private static SWTWorkbenchBot bot;
 
+	/** Dismisses the startup screen from eclipse and setups the bot. */
 	@BeforeClass
 	public static void beforeClass() throws Exception {
-		// don't use SWTWorkbenchBot here which relies on Platform 3.x
 		bot = new SWTWorkbenchBot();
 		bot.viewByTitle("Welcome").close();
 	}
 
+	/** Tests whether the command menu contribution from WatchDog active. */
 	@Test
 	public void testCommandMenuContribution() {
 		SWTBotMenu fileMenu = bot.menu("WatchDog");
 		Assert.assertNotNull(fileMenu);
-	}
-
-	@AfterClass
-	public static void sleep() {
-		bot.sleep(2000);
 	}
 
 }
