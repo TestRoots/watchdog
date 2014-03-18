ef4121e7da289ed856dc10441955faa01e63e18f
Georgios Gousios <gousiosg@gmail.com>
2014-03-18 21:05:57 +0100

    Initial implementation for adding users and intervals

--- WatchDogServer/spec/watchdog_server_spec.rb
+++ WatchDogServer/spec/watchdog_server_spec.rb
@@ -6,21 +6,29 @@ def app
   WatchDogServer
 end
 
-def test_user
+def test_user(unq)
   user = Hash.new
   user['email']       = 'foo@bar.gr'
   user['name']        = 'Foo Bar'
   user['org']         = 'Baz B.V.'
   user['org_website'] = 'http://baz.nl'
   user['prize']       = false
+  user['unq'] = if unq.nil? then (0...10).map{('a'..'z').to_a[rand(26)]}.join else unq end
   user
+end
+
+def test_interval(from, to)
+  interval = Hash.new
+  interval['ts'] = from
+  interval['te'] = to
+  interval
 end
 
 describe 'The WatchDog Server' do
 
-  before(:each) do
-    WatchDogServer.mongo.database
-  end
+  #before(:each) do
+  #  WatchDogServer.mongo.dele
+  #end
 
   it 'should woof' do
     get '/'
@@ -39,8 +47,50 @@ describe 'The WatchDog Server' do
     last_response.status.should eql(400)
   end
 
-  it 'should return the existing user when a user is re-added' do
-    post '/user', test_user.to_json
+  #it 'should return the existing user when a user is re-added' do
+  #  post '/user', test_user.to_json
+  #end
+
+  it 'should return 400 on bad JSON to /users/:id/intervals' do
+    post '/user/foobar/intervals', 'foobar'
+    last_response.status.should eql(400)
+  end
+
+  it 'should return 400 on non JSON array being sent to /users/:id/intervals' do
+    post '/user/foobar/intervals', '{"foo":"bar"}'
+    last_response.status.should eql(400)
+  end
+
+  it 'should return 400 when > 1000 intervals are sent at once' do
+    intervals = (1..1001).map{|x| test_interval(x, x + 1)}
+
+    post '/user/foobar/intervals', intervals.to_json
+    last_response.status.should eql(400)
+  end
+
+  it 'should return 400 when negative intervals exist' do
+    intervals = (1..10).map{|x| test_interval(x + 1, x)}
+
+    post '/user/foobar/intervals', intervals.to_json
+    last_response.status.should eql(400)
+  end
+
+  it 'should return 400 when posting intervals for non-existing user' do
+    intervals = (1..10).map{|x| test_interval(x, x + 1)}
+
+    post '/user/foobar/intervals', intervals.to_json
+    last_response.status.should eql(400)
+  end
+
+  it 'should return then number of stored intervals on successful insert' do
+    intervals = (1..10).map{|x| test_interval(x, x + 1)}
+    user = test_user('foobar')
+    post '/user', user.to_json
+
+    post '/user/foobar/intervals', intervals.to_json
+    last_response.status.should eql(201)
+    expect(last_response.body).to eq('10')
+
   end
 
 end
