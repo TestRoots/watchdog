53d1237d7461edf43ea0c30236b5d2c54f755ade
Moritz Beller <moritzbeller@gmx.de>
2014-04-10 17:34:15 +0200

    Makes WatchDog Registration pop up automatically on start of Eclipse
    Refs #36

--- WatchDog/src/nl/tudelft/watchdog/ui/handlers/StartUpHandler.java
+++ WatchDog/src/nl/tudelft/watchdog/ui/handlers/StartUpHandler.java
@@ -2,8 +2,12 @@ package nl.tudelft.watchdog.ui.handlers;
 
 import nl.tudelft.watchdog.logic.interval.IntervalManager;
 import nl.tudelft.watchdog.logic.logging.WatchDogLogger;
+import nl.tudelft.watchdog.ui.preferences.WatchdogPreferences;
 import nl.tudelft.watchdog.util.WatchDogGlobals;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IStartup;
 
 /**
@@ -18,6 +22,27 @@ public class StartUpHandler implements IStartup {
 		WatchDogGlobals.isActive = true;
 		WatchDogLogger.getInstance().logInfo("Starting WatchDog ...");
 		IntervalManager.getInstance();
-	}
 
+		Display.getDefault().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				if (WatchdogPreferences.getInstance().getUserid().isEmpty()) {
+					// if the UserID is empty, show WatchDog registration
+					// dialog.
+					UserWizardDialogHandler newUserWizardHandler = new UserWizardDialogHandler();
+					try {
+						newUserWizardHandler.execute(new ExecutionEvent());
+					} catch (ExecutionException exception) {
+						// TODO (MMB) Add a warning to the user here to manually
+						// execute registering?
+						WatchDogLogger
+								.getInstance()
+								.logInfo(
+										"Failed to display register dialog on startup!");
+					}
+				}
+			}
+		});
+	}
 }
