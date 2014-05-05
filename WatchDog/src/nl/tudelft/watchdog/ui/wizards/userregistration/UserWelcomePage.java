884d0f85a7e7923595fdf8b7158f7e6441bfabd6
Moritz Beller <moritzbeller@gmx.de>
2014-05-05 15:59:42 +0200

    Finishes project registration welcome page.

--- WatchDog/src/nl/tudelft/watchdog/ui/wizards/userregistration/UserWelcomePage.java
+++ WatchDog/src/nl/tudelft/watchdog/ui/wizards/userregistration/UserWelcomePage.java
@@ -17,6 +17,7 @@ class UserWelcomePage extends WelcomePage {
 		welcomeText = "WatchDog keeps track of the way you develop and test your software. Your usage data is sent to and maintained by the TestRoots team at Delft University. We are never going to do anything bad with it.\n\nYou can stay completely anonymous. But our research greatly improves, if you provide us with a bit of info about you. This way, you can also win one of our amazing prices.\n\nIf you want to know more about WatchDog (or the prices to win), visit our website <a href=\"http://watchdog.testroots.org\">watchdog.testroots.org</a>.";
 		labelText = "Your WatchDog User ID: ";
 		inputToolTip = "The User ID we sent you upon your first WatchDog registration.";
+		labelQuestion = "Do you already have a WatchDog UserId? ";
 	}
 
 }
