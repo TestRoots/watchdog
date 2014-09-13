0bed9c2ffee0c846c2d15bf6eda8d4211a512b07
Moritz Beller <moritzbeller@gmx.de>
2014-09-13 13:20:15 +0200

    Extracts Perspective Listener.

--- WatchDog/src/nl/tudelft/watchdog/logic/ui/listeners/WindowListener.java
+++ WatchDog/src/nl/tudelft/watchdog/logic/ui/listeners/WindowListener.java
@@ -4,8 +4,6 @@ import nl.tudelft.watchdog.logic.ui.EventManager;
 import nl.tudelft.watchdog.logic.ui.WatchDogEvent;
 import nl.tudelft.watchdog.logic.ui.WatchDogEvent.EventType;
 
-import org.eclipse.debug.ui.IDebugUIConstants;
-import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWindowListener;
@@ -61,33 +59,8 @@ public class WindowListener implements IWindowListener {
 	}
 
 	private void addPerspectiveListener(IWorkbenchWindow window) {
-		IPerspectiveListener perspectiveListener = new IPerspectiveListener() {
-
-			@Override
-			public void perspectiveChanged(IWorkbenchPage page,
-					IPerspectiveDescriptor perspective, String changeId) {
-				// intentionally left empty
-			}
-
-			@Override
-			public void perspectiveActivated(IWorkbenchPage page,
-					IPerspectiveDescriptor perspective) {
-				switch (perspective.getId()) {
-				case IDebugUIConstants.ID_DEBUG_PERSPECTIVE:
-					eventManager.update(new WatchDogEvent(perspective,
-							EventType.START_DEBUG_PERSPECTIVE));
-					break;
-				case JavaUI.ID_PERSPECTIVE:
-					eventManager.update(new WatchDogEvent(perspective,
-							EventType.START_JAVA_PERSPECTIVE));
-					break;
-				default:
-					eventManager.update(new WatchDogEvent(perspective.getId(),
-							EventType.START_UNKNOWN_PERSPECTIVE));
-
-				}
-			}
-		};
+		IPerspectiveListener perspectiveListener = new PerspectiveListener(
+				eventManager);
 		window.addPerspectiveListener(perspectiveListener);
 
 		// triggers the event for the currently open perspective, if there is
