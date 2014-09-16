8c46ef643472352e3b3028d63684f815b3f63177
Moritz Beller <moritzbeller@gmx.de>
2014-09-16 13:11:20 +0200

    Bumps server version.
    Refs #83

--- WatchDogServer/lib/watchdog_server.rb
+++ WatchDogServer/lib/watchdog_server.rb
@@ -46,7 +46,7 @@ class WatchDogServer < Sinatra::Base
 
   # Get info about stored user
   get '/client' do
-    client_version = "1.0-RC"
+    client_version = "1.0"
 
     status 200
     body client_version.to_json
