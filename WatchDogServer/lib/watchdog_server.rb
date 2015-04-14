a08c7a95e15d0d3ff27d49c55826be8465310dc8
Moritz Beller <moritzbeller@gmx.de>
2015-04-14 17:51:55 +0200

    Closes #159

--- WatchDogServer/lib/watchdog_server.rb
+++ WatchDogServer/lib/watchdog_server.rb
@@ -50,7 +50,7 @@ class WatchDogServer < Sinatra::Base
 
   # Get info about stored user
   get '/client' do
-    client_version = "1.3.0"
+    client_version = "1.4.0"
 
     status 200
     body client_version.to_json
