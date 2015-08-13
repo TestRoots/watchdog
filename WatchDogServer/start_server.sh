c93ef191be162d274838dbe6ec4508c896466998
Georgios Gousios <gousiosg@gmail.com>
2015-08-13 21:57:03 +0200

    Adds modified supervise-able scripts.

--- WatchDogServer/start_server.sh
+++ WatchDogServer/start_server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+cd .
 if [ -z $RACK_ENV ]; then
     export RACK_ENV="production"
 fi
 echo "Starting Watchdog with RACK_ENV $RACK_ENV"
-unicorn --env $RACK_ENV -c unicorn.rb -D
+unicorn --env $RACK_ENV -c unicorn.rb 
