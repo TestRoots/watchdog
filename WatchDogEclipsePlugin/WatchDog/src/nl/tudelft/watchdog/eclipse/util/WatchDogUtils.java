37685b9bb6886b535f7a48bf7d7e240dcf35d5ee
Moritz Beller <moritzbeller@gmx.de>
2015-11-29 17:52:18 +0100

    Removes dead code.

--- WatchDogEclipsePlugin/WatchDog/src/nl/tudelft/watchdog/eclipse/util/WatchDogUtils.java
+++ WatchDogEclipsePlugin/WatchDog/src/nl/tudelft/watchdog/eclipse/util/WatchDogUtils.java
@@ -3,12 +3,6 @@ package nl.tudelft.watchdog.eclipse.util;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import nl.tudelft.watchdog.core.ui.preferences.ProjectPreferenceSetting;
-import nl.tudelft.watchdog.core.util.ContentReaderException;
-import nl.tudelft.watchdog.core.util.WatchDogLogger;
-import nl.tudelft.watchdog.core.util.WatchDogUtilsBase;
-import nl.tudelft.watchdog.eclipse.ui.preferences.Preferences;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.text.IDocument;
@@ -16,22 +10,15 @@ import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
-import org.joda.time.Duration;
-import org.joda.time.Period;
-import org.joda.time.format.PeriodFormatter;
-import org.joda.time.format.PeriodFormatterBuilder;
+
+import nl.tudelft.watchdog.core.ui.preferences.ProjectPreferenceSetting;
+import nl.tudelft.watchdog.core.util.ContentReaderException;
+import nl.tudelft.watchdog.core.util.WatchDogLogger;
+import nl.tudelft.watchdog.core.util.WatchDogUtilsBase;
+import nl.tudelft.watchdog.eclipse.ui.preferences.Preferences;
 
 /** Utilities for WatchDog. */
 public class WatchDogUtils extends WatchDogUtilsBase {
-
-	/** Formatter for a {@link Period}. */
-	private static PeriodFormatter periodFormatter = new PeriodFormatterBuilder()
-			.printZeroRarelyLast().appendDays().appendSuffix(" day", " days")
-			.appendSeparator(", ").appendHours()
-			.appendSuffix(" hour", " hours").appendSeparator(", ")
-			.appendMinutes().appendSuffix(" minute", " minutes")
-			.appendSeparator(" and ").appendSeconds()
-			.appendSuffix(" second", " seconds").toFormatter();
 
 	/**
 	 * Returns the contents of the supplied {@link ITextEditor}.
@@ -70,8 +57,8 @@ public class WatchDogUtils extends WatchDogUtilsBase {
 					"Editor closed: Document provider is null");
 		}
 
-		IDocument document = documentProvider.getDocument(editor
-				.getEditorInput());
+		IDocument document = documentProvider
+				.getDocument(editor.getEditorInput());
 		return document;
 	}
 
@@ -124,15 +111,6 @@ public class WatchDogUtils extends WatchDogUtilsBase {
 		}
 	}
 
-	/**
-	 * @return the converted {@link Duration} in a human-readable String,
-	 *         discarding milliseconds.
-	 */
-	public static String makeDurationHumanReadable(Duration duration) {
-		return periodFormatter.print(duration.toPeriod().withMillis(0)
-				.normalizedStandard());
-	}
-
 	/** Sleeps for the specified amount of milliseconds. */
 	public static void sleep(long milis) {
 		try {
@@ -153,8 +131,8 @@ public class WatchDogUtils extends WatchDogUtilsBase {
 	 * workspace.
 	 */
 	public static ProjectPreferenceSetting getProjectSetting() {
-		return Preferences.getInstance().getOrCreateProjectSetting(
-				getWorkspaceName());
+		return Preferences.getInstance()
+				.getOrCreateProjectSetting(getWorkspaceName());
 	}
 
 }
--- WatchDogEclipsePlugin/WatchDogUnitTests/src/nl/tudelft/watchdog/util/WatchDogUtilsTest.java
+++ WatchDogEclipsePlugin/WatchDogUnitTests/src/nl/tudelft/watchdog/util/WatchDogUtilsTest.java
@@ -2,53 +2,15 @@ package nl.tudelft.watchdog.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import nl.tudelft.watchdog.eclipse.util.WatchDogUtils;
 
-import org.joda.time.Duration;
 import org.junit.Test;
+
+import nl.tudelft.watchdog.eclipse.util.WatchDogUtils;
 
 /**
  * Tests for the {@link WatchDogUtils}.
  */
 public class WatchDogUtilsTest {
-
-	/**
-	 * Tests the human readable durations.
-	 */
-	@Test
-	public void testConvertJodaDurationToReadableString() {
-		Duration duration = new Duration(0);
-		assertEquals("0 seconds",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(999);
-		assertEquals("0 seconds",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(1000);
-		assertEquals("1 second",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(1010);
-		assertEquals("1 second",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(1999);
-		assertEquals("1 second",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(2034);
-		assertEquals("2 seconds",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(11111111L);
-		assertEquals("3 hours, 5 minutes and 11 seconds",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-
-		duration = new Duration(848654688715648L);
-		assertEquals("6 days, 5 hours, 31 minutes and 55 seconds",
-				WatchDogUtils.makeDurationHumanReadable(duration));
-	}
 
 	@Test
 	public void testIsEmpty() {
