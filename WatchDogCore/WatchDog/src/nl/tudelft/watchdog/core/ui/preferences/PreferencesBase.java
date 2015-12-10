820a0af28e0c55aa7707167abe07147c0e691dfb
Niels Spruit <spruit.niels@gmail.com>
2015-12-10 16:51:59 +0100

    Pulled duplicate code into PreferencesBase
    Refs #219

--- WatchDogCore/WatchDog/src/nl/tudelft/watchdog/core/ui/preferences/PreferencesBase.java
+++ WatchDogCore/WatchDog/src/nl/tudelft/watchdog/core/ui/preferences/PreferencesBase.java
@@ -1,87 +1,131 @@
 package nl.tudelft.watchdog.core.ui.preferences;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /** Skeleton for which preferences each plugin must provide. */
-public interface PreferencesBase {
+public abstract class PreferencesBase {
+	
+	/** The map of registered workspaces. */
+	protected List<ProjectPreferenceSetting> projectSettings = new ArrayList<ProjectPreferenceSetting>();
 	
 	/**
 	 * Returns whether logging is enabled (<code>true</code>) or not (
 	 * <code>false</code>).
 	 */
-    boolean isLoggingEnabled();
+    public abstract boolean isLoggingEnabled();
 
 	/**
 	 * Returns whether authentication on the url is enabled (<code>true</code>)
 	 * or not ( <code>false</code>).
 	 */
-    boolean isAuthenticationEnabled();
+    public abstract boolean isAuthenticationEnabled();
 
     /** @return The userid. */
-    String getUserId();
+    public abstract String getUserId();
 
-    void setUserId(String userid);
+    public abstract void setUserId(String userid);
 
 	/** @return Whether this client version is outdated. */
-    Boolean isOldVersion();
+    public abstract Boolean isOldVersion();
 
 	/** Sets whether this client version is outdated. */
-    void setIsOldVersion(Boolean outdated);
+    public abstract void setIsOldVersion(Boolean outdated);
 
 	/** @return Whether this client version is outdated. */
-    Boolean isBigUpdateAvailable();
+    public abstract Boolean isBigUpdateAvailable();
 
 	/** Sets whether this client version has a big update available. */
-    void setBigUpdateAvailable(Boolean available);
+    public abstract void setBigUpdateAvailable(Boolean available);
 
 	/** @return Whether the user answered to the big update question. */
-    Boolean isBigUpdateAnswered();
+    public abstract Boolean isBigUpdateAnswered();
 
 	/** Sets whether this client version has a big update available. */
-    void setBigUpdateAnswered(Boolean answered);
+    public abstract void setBigUpdateAnswered(Boolean answered);
 
-    long getIntervals();
+    public abstract long getIntervals();
 
 	/** Adds the number to the transfered intervals for the store. */
-    void addTransferedIntervals(long number);
+    public abstract void addTransferedIntervals(long number);
 
 	/** @return The number of successfully transfered intervals. */
-    String getLastIntervalTransferDate();
+    public abstract String getLastIntervalTransferDate();
 
 	/** Adds the number to the transfered intervals for the store. */
-    void setLastTransferedInterval();
+    public abstract void setLastTransferedInterval();
 
 	/** @return The serverURL. */
-    String getServerURI();
+    public abstract String getServerURI();
 
 	/**
 	 * @return <code>true</code> if this workspace has already been registered
 	 *         with WatchDog, <code>false</code> otherwise. Note: This does not
 	 *         say whether WatchDog should be activated.}.
 	 */
-    boolean isProjectRegistered(String project);
+    public boolean isProjectRegistered(String project) {
+    	ProjectPreferenceSetting projectSetting = getProjectSetting(
+				project);
+		return (projectSetting != null
+				&& projectSetting.startupQuestionAsked);
+    }
 
 	/**
 	 * @return The matching {@link ProjectPreferenceSetting}, or a completely
 	 *         new one in case there was no match.
 	 */
-    ProjectPreferenceSetting getOrCreateProjectSetting(String project);
+    public ProjectPreferenceSetting getOrCreateProjectSetting(String project) {
+    	ProjectPreferenceSetting setting = getProjectSetting(project);
+		if (setting == null) {
+			setting = new ProjectPreferenceSetting();
+			setting.project = project;
+			projectSettings.add(setting);
+		}
+		return setting;
+    }
+    
+    /**
+	 * @return The matching {@link ProjectPreferenceSetting}, or
+	 *         <code>null</code> in case there was no match.
+	 */
+	private ProjectPreferenceSetting getProjectSetting(String project) {
+		for (ProjectPreferenceSetting setting : projectSettings) {
+			if (setting.project.equals(project)) {
+				return setting;
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Registers the given workspace with WatchDog. If use is <code>true</code>,
 	 * WatchDog will be used.
 	 */
-    void registerProjectUse(String project, boolean use);
+    public void registerProjectUse(String project, boolean use) {
+		ProjectPreferenceSetting setting = getOrCreateProjectSetting(project);
+		setting.enableWatchdog = use;
+		setting.startupQuestionAsked = true;
+		storeProjectSettings();
+	}
 
-	/** Registers the given projectId with the given workspace. */
-    void registerProjectId(String project, String projectId);
+	/** Registers the given projectId with the given project. */
+    public void registerProjectId(String project, String projectId) {
+		ProjectPreferenceSetting setting = getOrCreateProjectSetting(project);
+		setting.projectId = projectId;
+		storeProjectSettings();
+	}
 
-	/** @return a list of workspace settings. */
-    List<ProjectPreferenceSetting> getProjectSettings();
+    /** Updates the serialized project settings in the preference store. */
+    protected abstract void storeProjectSettings();
+    
+	/** @return a list of project settings. */
+    public List<ProjectPreferenceSetting> getProjectSettings() {
+		return projectSettings;
+	}
 
 	/**
 	 * Resets certain WatchDog values to the default which are only used
 	 * internally.
 	 */
-    void setDefaults();
+    public abstract void setDefaults();
 }
