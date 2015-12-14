159379bf5045c9df58362209e0f3c97609a8b93f
mjduijn <maartenduijn@msn.com>
2015-12-14 20:34:07 +0100

    Copy artifacts to home

--- travis_build.sh
+++ travis_build.sh
@@ -39,5 +39,9 @@ ECLIPSE_CLIENT_STATUS=$?
 cd ..
 
 git ls-files -o
+cp /home/travis/build/mjduijn/watchdog/WatchDogCore/WatchDog/target/nl.tudelft.WatchDogCore-1.6.0.jar WatchDogCore-1.6.0.jar
+cp /home/travis/build/mjduijn/watchdog/WatchDogIntelliJPlugin/WatchDog/target/lib/nl.tudelft.WatchDog-1.6.0.jar WatchDogIntelliJPlugin-1.6.0.jar
+cp /home/travis/build/mjduijn/watchdog/WatchDogIntelliJPlugin/WatchDog/target/nl.tudelft.WatchDog-1.6.0-plugin.zip WatchDogIntelliJPlugin-1.6.0.zip
+cp /home/travis/build/mjduijn/watchdog/WatchDogEclipsePlugin/WatchDog/target/nl.tudelft.WatchDog-1.6.0.jar WatchDogEclipsePlugin-1.6.0.jar
 
 exit $(($SERVER_STATUS + $CORE_STATUS + $INTELLIJ_CLIENT_STATUS + $ECLIPSE_CLIENT_STATUS))
