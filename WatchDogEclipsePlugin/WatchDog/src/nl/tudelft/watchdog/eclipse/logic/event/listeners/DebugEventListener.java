88538fa15770f8c8c1647aac877fae2e054f530c
Niels Spruit <niellus16@hotmail.com>
2016-03-31 12:35:14 +0200

    Events are now generated for inspecting/modifying vars and defining watches in Eclipse
    Refs #3

--- WatchDogEclipsePlugin/WatchDog/src/nl/tudelft/watchdog/eclipse/logic/event/listeners/DebugEventListener.java
+++ WatchDogEclipsePlugin/WatchDog/src/nl/tudelft/watchdog/eclipse/logic/event/listeners/DebugEventListener.java
@@ -1,10 +1,14 @@
 package nl.tudelft.watchdog.eclipse.logic.event.listeners;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.model.IThread;
+import org.eclipse.debug.core.model.IVariable;
+import org.eclipse.debug.core.model.IWatchExpression;
 
 import nl.tudelft.watchdog.core.logic.event.EventManager;
 import nl.tudelft.watchdog.core.logic.event.eventtypes.DebugEventBase;
@@ -23,9 +27,16 @@ public class DebugEventListener implements IDebugEventSetListener {
 	 */
 	private final EventManager eventManager;
 
+	/**
+	 * List with the hashes of all current watch expressions. Used to avoid
+	 * duplicate 'Define Watch' events.
+	 */
+	private List<Integer> watchExpressionHashes;
+
 	/** Constructor. */
 	public DebugEventListener(EventManager eventManager) {
 		this.eventManager = eventManager;
+		this.watchExpressionHashes = new ArrayList<>();
 	}
 
 	@Override
@@ -33,46 +44,76 @@ public class DebugEventListener implements IDebugEventSetListener {
 		for (DebugEvent event : events) {
 			if (event.getSource() instanceof IThread) {
 				handleDebugEvent(event);
+			} else if (event.getSource() instanceof IWatchExpression) {
+				handleWatchExpressionEvent(event);
+			} else if (event.getSource() instanceof IVariable
+					&& event.getKind() == DebugEvent.CHANGE
+					&& event.getDetail() == DebugEvent.CONTENT) {
+				eventManager.addEvent(new DebugEventBase(
+						EventType.MODIFY_VARIABLE_VALUE, new Date()));
 			}
 		}
 	}
 
 	/**
-	 * Creates the correct {@link DebugEventBase} instance based on the input
-	 * event's properties.
+	 * Creates the correct {@link DebugEventBase} instance for SUSPEND and
+	 * RESUME events based on the input event's properties.
 	 */
 	private void handleDebugEvent(DebugEvent event) {
 		if (event.getKind() == DebugEvent.SUSPEND) {
-			switch (event.getDetail()) {
-			case DebugEvent.BREAKPOINT:
-				eventManager.addEvent(new DebugEventBase(
-						EventType.SUSPEND_BREAKPOINT, new Date()));
-				break;
-			case DebugEvent.CLIENT_REQUEST:
-				eventManager.addEvent(new DebugEventBase(
-						EventType.SUSPEND_CLIENT, new Date()));
-				break;
-			}
+			handleSuspendEvent(event);
 		} else if (event.getKind() == DebugEvent.RESUME) {
-			switch (event.getDetail()) {
-			case DebugEvent.STEP_INTO:
-				eventManager.addEvent(
-						new DebugEventBase(EventType.STEP_INTO, new Date()));
-				break;
-			case DebugEvent.STEP_OVER:
-				eventManager.addEvent(
-						new DebugEventBase(EventType.STEP_OVER, new Date()));
-				break;
-			case DebugEvent.STEP_RETURN:
-				eventManager.addEvent(
-						new DebugEventBase(EventType.STEP_OUT, new Date()));
-				break;
-			case DebugEvent.CLIENT_REQUEST:
-				eventManager.addEvent(new DebugEventBase(
-						EventType.RESUME_CLIENT, new Date()));
-				break;
-			}
+			handleResumeEvent(event);
 		}
 	}
 
+	private void handleSuspendEvent(DebugEvent event) {
+		switch (event.getDetail()) {
+		case DebugEvent.BREAKPOINT:
+			eventManager.addEvent(new DebugEventBase(
+					EventType.SUSPEND_BREAKPOINT, new Date()));
+			break;
+		case DebugEvent.CLIENT_REQUEST:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.SUSPEND_CLIENT, new Date()));
+			break;
+		case DebugEvent.EVALUATION:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.INSPECT_VARIABLE, new Date()));
+			break;
+		}
+	}
+
+	private void handleResumeEvent(DebugEvent event) {
+		switch (event.getDetail()) {
+		case DebugEvent.STEP_INTO:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.STEP_INTO, new Date()));
+			break;
+		case DebugEvent.STEP_OVER:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.STEP_OVER, new Date()));
+			break;
+		case DebugEvent.STEP_RETURN:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.STEP_OUT, new Date()));
+			break;
+		case DebugEvent.CLIENT_REQUEST:
+			eventManager.addEvent(
+					new DebugEventBase(EventType.RESUME_CLIENT, new Date()));
+			break;
+		}
+	}
+
+	/**
+	 * Handles all watch expression events. Makes sure that only one 'Define
+	 * Watch' event is generated for each expression.
+	 */
+	private void handleWatchExpressionEvent(DebugEvent event) {
+		if (!watchExpressionHashes.contains(event.getSource().hashCode())) {
+			eventManager.addEvent(
+					new DebugEventBase(EventType.DEFINE_WATCH, new Date()));
+			watchExpressionHashes.add(event.getSource().hashCode());
+		}
+	}
 }
