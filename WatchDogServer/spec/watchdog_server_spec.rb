92da2d04568d8df153d2242ce7392bfdfbee1072
Moritz Beller <moritzbeller@gmx.de>
2016-05-25 17:00:45 +0200

    Bumps version to 2.0.1
    Integrates default-anonymous registration.

--- WatchDogServer/spec/watchdog_server_spec.rb
+++ WatchDogServer/spec/watchdog_server_spec.rb
@@ -69,7 +69,7 @@ describe 'The WatchDog Server' do
   it 'should get client version' do
     get '/client'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq('"2.0.0"')
+    expect(last_response.body).to eq('"2.0.1"')
   end
 
   it 'should create a user when the details are correct' do
