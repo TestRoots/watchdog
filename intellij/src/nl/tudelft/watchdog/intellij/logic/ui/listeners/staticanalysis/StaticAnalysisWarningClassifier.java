73af8e41b3606db98b630815a8d9dc2ecf6953ab
Tim van der Lippe <tvanderlippe@gmail.com>
2018-03-06 13:10:17 +0100

    Optimize classification in intellij

--- intellij/src/nl/tudelft/watchdog/intellij/logic/ui/listeners/staticanalysis/StaticAnalysisWarningClassifier.java
+++ intellij/src/nl/tudelft/watchdog/intellij/logic/ui/listeners/staticanalysis/StaticAnalysisWarningClassifier.java
@@ -1,37 +1,54 @@
 package nl.tudelft.watchdog.intellij.logic.ui.listeners.staticanalysis;
 
 import nl.tudelft.watchdog.core.util.WatchDogLogger;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.regex.Pattern;
 
-public class StaticAnalysisWarningClassifier {
+class StaticAnalysisWarningClassifier {
 
-    // TODO(timvdlippe): Split up for full static strings and those that have variable patterns in it.
-    // E.g. anything with '{\d+}' is a pattern, everything else just a static string.
-    private static Map<Pattern, String> reverseKeySet = new HashMap<>();
+    /**
+     * Use a pattern instead of {@link String#matches(String)}, because matches wants to match the whole
+     * string, and we need to do a "contains" instead.
+     */
+    private static final Pattern BRACKETS_PATTERN = Pattern.compile("\\{.+}");
+
+    private static final Map<String, String> staticKeyMap = new HashMap<>();
+    private static final List<PatternBasedKey> patternBasedKeyList = new ArrayList<>();
 
     static {
         createPatternsForKeysInBundle(ResourceBundle.getBundle("messages.InspectionsBundle"));
         createPatternsForKeysInBundle(ResourceBundle.getBundle("com.siyeh.InspectionGadgetsBundle"));
+
+        // Sort the list on longest pattern first
+        Collections.sort(patternBasedKeyList);
     }
 
     private static void createPatternsForKeysInBundle(ResourceBundle bundle) {
         bundle.keySet().forEach(key -> {
             try {
-                final String textualDescription = bundle.getString(key)
-                        .replaceAll("''", "'")
-                        .replaceAll("\\(", "\\(")
-                        .replaceAll("\\)", "\\)")
-                        .replaceAll("<code>#ref</code>", "'[^']+'")
-                        .replaceAll("\\{.+}", ".+")
-                        .replaceAll(" #loc", "")
-                        .replaceAll(" #ref", "");
+                final String message = bundle.getString(key);
 
-                if (!".+".equals(textualDescription)) {
-                    reverseKeySet.put(Pattern.compile(textualDescription), key);
+                if (containsDynamicParts(message)) {
+                    final String regex = message.replaceAll("''", "'")
+                            .replaceAll("\\(", "\\\\(")
+                            .replaceAll("\\)", "\\\\)")
+                            .replaceAll("\\[", "\\\\[")
+                            .replaceAll("<code>[^<]+</code>", "'[^']+'")
+                            // Perform the next replace twice, as there can be nested brackets. For example:
+                            // {1, choice, 1#direct or indirect implementation|2#{1,number} direct or indirect implementations}
+                            .replaceAll("\\{[^{}]+}", ".+")
+                            .replaceAll("\\{[^{}]+}", ".+")
+                            .replaceAll(" #loc", "")
+                            .replaceAll(" #ref", "");
+
+                    // Filter out the "catch-all" patterns. These would almost always be erroneous matches.
+                    if (!".+".equals(regex)) {
+                        patternBasedKeyList.add(new PatternBasedKey(Pattern.compile(regex), key));
+                    }
+                } else {
+                    staticKeyMap.put(message, key);
                 }
             } catch (Exception ignored) {
                 WatchDogLogger.getInstance().logSevere("Could not create pattern for key \"" + key + "\"");
@@ -39,11 +56,45 @@ public class StaticAnalysisWarningClassifier {
         });
     }
 
-    public static String classify(String message) {
-        return reverseKeySet.entrySet().stream()
-                .filter(entry -> entry.getKey().matcher(message).matches())
-                .map(Map.Entry::getValue)
-                .findFirst()
-                .orElse("UNKNOWN");
+    private static boolean containsDynamicParts(String message) {
+        return message.contains("#loc")
+                || message.contains("#ref")
+                || BRACKETS_PATTERN.matcher(message).find();
+    }
+
+    static String classify(String message) {
+        // For performance reasons, we first do a static lookup. In this case, the message
+        // does not contain any dynamic parts. This lookup is O(1) and catches most of the cases.
+        // If instead the message contains dynamic parts, we have to pattern match in the other list.
+        // This list is sorted on longest patterns first, to make sure we are most-specific first.
+        //
+        // Use the Java Stream API with "findFirst", to make sure we bail out as soon as possible,
+        // to not do any unnecessary work.
+        return staticKeyMap.getOrDefault(message,
+                patternBasedKeyList.stream()
+                        .filter(key -> key.pattern.matcher(message).matches())
+                        .map(key -> key.key)
+                        .findFirst()
+                        .orElse("unknown")
+        );
+    }
+
+    private static final class PatternBasedKey implements Comparable<PatternBasedKey> {
+        private final Pattern pattern;
+        private final String key;
+        private final int length;
+
+        PatternBasedKey(Pattern pattern, String key) {
+            this.pattern = pattern;
+            this.key = key;
+            this.length = pattern.pattern().length();
+        }
+
+        @Override
+        public int compareTo(@NotNull PatternBasedKey other) {
+            // Reverse the order of this and other here, because we want
+            // the list to be sorted from largest pattern to shortest.
+            return Integer.compare(other.length, this.length);
+        }
     }
 }
