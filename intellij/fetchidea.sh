4aeeb50445f83e28c484d176024e05bb6f31473c
Tim van der Lippe <tvanderlippe@gmail.com>
2018-03-29 13:27:41 +0200

    Use CheckStylePluginApi to be forwards-compatible

--- intellij/fetchidea.sh
+++ intellij/fetchidea.sh
@@ -44,7 +44,7 @@ mvn install:install-file -Dmaven.exec.skip -Dfile=$build_dir/$idea_directory.zip
 
 cd $build_dir
 
-checkstyle_version="5.18.3"
+checkstyle_version="5.18.5"
 checkstyle_directory_name="CheckStyle-"
 checkstyle_zip="$checkstyle_directory_name$checkstyle_version.zip"
 checkstyle_idea_URL="http://plugins.jetbrains.com/plugin/download?rel=true&updateId=44080"
