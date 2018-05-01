80450ae8552899b83e5458195b00a1706678f914
Tim van der Lippe <tvanderlippe@gmail.com>
2018-05-01 16:42:59 +0200

    Fix exception when an editor is closed before the analyzer has finished

--- intellij/src/nl/tudelft/watchdog/intellij/logic/ui/listeners/staticanalysis/IntelliJMarkupModelListener.java
+++ intellij/src/nl/tudelft/watchdog/intellij/logic/ui/listeners/staticanalysis/IntelliJMarkupModelListener.java
@@ -16,6 +16,7 @@ import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.fileEditor.FileDocumentManagerAdapter;
 import com.intellij.openapi.project.DumbServiceImpl;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.util.messages.MessageBusConnection;
 import nl.tudelft.watchdog.core.logic.document.Document;
 import nl.tudelft.watchdog.core.logic.event.TrackingEventManager;
@@ -85,7 +86,13 @@ public class IntelliJMarkupModelListener extends CoreMarkupModelListener impleme
         // Therefore, wait for smart mode and only then start listening, to make sure the codeAnalyzer actually did its thing.
         // For more information see https://www.jetbrains.org/intellij/sdk/docs/basics/indexing_and_psi_stubs.html
         DumbServiceImpl.getInstance(project).runWhenSmart(() -> {
-            final DaemonCodeAnalyzerImpl analyzer = (DaemonCodeAnalyzerImpl) DaemonCodeAnalyzer.getInstance(project);
+        	// In the case that the analyzer finished after a file was closed just after opening the IDE,
+			// do not try to attach any other listeners. These listeners would fail, as the editor no longer exists.
+        	if (Disposer.isDisposed(disposable)) {
+        		return;
+			}
+
+			final DaemonCodeAnalyzerImpl analyzer = (DaemonCodeAnalyzerImpl) DaemonCodeAnalyzer.getInstance(project);
             final MessageBusConnection codeAnalyzerMessageBusConnection = project.getMessageBus().connect(disposable);
             final MessageBusConnection documentMessageBusConnection = project.getMessageBus().connect(disposable);
 
