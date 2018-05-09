f2c751678e03a5a7ad8ee7afb4c8c34861026273
Tim van der Lippe <tvanderlippe@gmail.com>
2018-05-09 11:50:46 +0200

    Add clarification documentation and rename constructor parameter

--- core/src/nl/tudelft/watchdog/core/logic/ui/listeners/staticanalysis/Warning.java
+++ core/src/nl/tudelft/watchdog/core/logic/ui/listeners/staticanalysis/Warning.java
@@ -16,8 +16,8 @@ public class Warning<T> implements Serializable {
 
 	private static final long serialVersionUID = -573132089990619360L;
 
-	@SerializedName("docline")
-	public final int docLineNumber;
+	@SerializedName("doctotal")
+	public final int docTotalLines;
 
 	@SerializedName("type")
 	public final T type;
@@ -31,12 +31,29 @@ public class Warning<T> implements Serializable {
 	@SerializedName("diff")
 	public final int secondsBetween;
 
-	public Warning(int docLineNumber, T type, int lineNumber, Date warningCreationTime) {
-		this(docLineNumber, type, lineNumber, warningCreationTime, -1);
+	/**
+	 * Create a warning in a document, without a difference in time of a previous warning event
+	 * (in the case this event is a warning deletion).
+	 * @param docTotalLines The total number of lines in the document at the moment the warning was created.
+	 * @param type The type of warning, could be a highlighter or the textual classification of the warning.
+	 * @param lineNumber The line number this warning was created on.
+	 * @param warningCreationTime The timestamp this warning was created at.
+	 */
+	public Warning(int docTotalLines, T type, int lineNumber, Date warningCreationTime) {
+		this(docTotalLines, type, lineNumber, warningCreationTime, -1);
 	}
 
-	public Warning(int docLineNumber, T type, int lineNumber, Date warningCreationTime, int secondsBetween) {
-		this.docLineNumber = docLineNumber;
+	/**
+	 * Create a warning in a document, including a difference in time of a previous warning event
+	 * (in the case this event is a warning deletion).
+	 * @param docTotalLines The total number of lines in the document at the moment the warning was created.
+	 * @param type The type of warning, could be a highlighter or the textual classification of the warning.
+	 * @param lineNumber The line number this warning was created on.
+	 * @param warningCreationTime The timestamp this warning was created at.
+	 * @param secondsBetween The number of seconds between a warning deletion and the original warning creation timestamp.
+	 */
+	public Warning(int docTotalLines, T type, int lineNumber, Date warningCreationTime, int secondsBetween) {
+		this.docTotalLines = docTotalLines;
 		this.type = type;
 		this.warningCreationTime = warningCreationTime;
 		this.secondsBetween = secondsBetween;
