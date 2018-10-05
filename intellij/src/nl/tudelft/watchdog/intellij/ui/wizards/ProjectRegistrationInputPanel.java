1a785af066e9e11993b8f5b52f880fb092538395
Moritz Beller (Inventitech) <moritzbeller@gmx.de>
2018-10-05 18:05:35 +0200

    Add slider for IntelliJ
    Refs #318

--- intellij/src/nl/tudelft/watchdog/intellij/ui/wizards/ProjectRegistrationInputPanel.java
+++ intellij/src/nl/tudelft/watchdog/intellij/ui/wizards/ProjectRegistrationInputPanel.java
@@ -3,14 +3,17 @@ package nl.tudelft.watchdog.intellij.ui.wizards;
 import nl.tudelft.watchdog.core.logic.network.JsonTransferer;
 import nl.tudelft.watchdog.core.ui.wizards.Project;
 import nl.tudelft.watchdog.intellij.ui.preferences.Preferences;
+import nl.tudelft.watchdog.intellij.ui.util.UIUtils;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 
 import static nl.tudelft.watchdog.core.ui.wizards.Project.*;
 import static nl.tudelft.watchdog.core.ui.wizards.WizardStrings.INPUT_IS_OPTIONAL;
-import static nl.tudelft.watchdog.intellij.ui.wizards.WizardStep.DEFAULT_SPACING;
 
 class ProjectRegistrationInputPanel extends RegistrationInputPanel {
 
@@ -22,8 +25,20 @@ class ProjectRegistrationInputPanel extends RegistrationInputPanel {
     private ButtonGroup automationUsage;
     private JTextField toolsUsed;
 
+    private final JPanel slider;
+
+    private Boolean sliderTouched = false;
+
+    /**
+     * The slider. Its value denotes in full percentage how much production code
+     * the user estimates to write.
+     */
+    protected JSlider percentageProductionSlider;
+    private int productionPercentageStart;
+
     /**
      * A panel to ask the user questions regarding their project.
+     *
      * @param callback The callback invoked after the user clicked "Create WatchDog Project".
      */
     ProjectRegistrationInputPanel(Consumer<Boolean> callback) {
@@ -33,21 +48,28 @@ class ProjectRegistrationInputPanel extends RegistrationInputPanel {
         JPanel introductionContainer = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 3));
         this.add(introductionContainer);
         introductionContainer.add(new JLabel("<html>" +
-                "<h3>" + WATCHDOG_PROJECT_PROFILE + "</h3>" +
-                PROJECT_DATA_REQUEST + "<br>" +
-                INPUT_IS_OPTIONAL));
+            "<h3>" + WATCHDOG_PROJECT_PROFILE + "</h3>" +
+            PROJECT_DATA_REQUEST + "<br>" +
+            INPUT_IS_OPTIONAL));
 
-		this.inputContainer = new JPanel(new GridLayout(0, 2));
-		this.add(inputContainer);
+        JPanel questionContainer = new JPanel(new GridLayout(0, 2));
+        this.add(questionContainer);
 
-        this.createInputFields();
+        this.createInputFields(questionContainer);
 
-        this.add(Box.createVerticalStrut(DEFAULT_SPACING));
+        this.add(Box.createVerticalStrut(20));
+
+        this.slider = new JPanel(new GridLayout(0, 1));
+        this.createSlider(slider);
+        this.add(slider);
+
+        this.inputContainer = new JPanel(new GridLayout(0, 2));
+        this.add(inputContainer);
 
         this.createButtonAndStatusContainer(CREATE_PROJECT_BUTTON_TEXT);
     }
 
-    private void createInputFields() {
+    private void createInputFields(JPanel inputContainer) {
         this.projectName = WizardStep.createLinkedLabelTextField(PROJECT_NAME_LABEL, PROJECT_NAME_TEXTFIELD_TOOLTIP, 150, inputContainer);
         this.projectWebsite = WizardStep.createLinkedLabelTextField(PROJECT_WEBSITE_LABEL, PROJECT_WEBSITE_TEXTFIELD_TOOLTIP, 150, inputContainer);
         this.ciUsage = WizardStep.createYesNoDontKnowQuestionWithLabel(CI_USAGE_LABEL_TEXT, inputContainer);
@@ -62,8 +84,87 @@ class ProjectRegistrationInputPanel extends RegistrationInputPanel {
         this.toolsUsed = WizardStep.createLinkedLabelTextField(TOOL_USAGE_LABEL_TEXT, TOOL_USAGE_TEXTFIELD_TOOLTIP, 150, inputContainer);
     }
 
+    private void createSlider(JPanel parent) {
+        JPanel questionPanel = UIUtils.createFlowJPanelLeft(parent);
+
+        UIUtils.createLabel(questionPanel,
+            "Estimate how you divide your time into the two activities testing and production. Just have a wild guess!");
+
+        this.productionPercentageStart = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+
+        JPanel sliderPanel = UIUtils.createFlowJPanelCenter(parent);
+        JPanel row = UIUtils.createFlowJPanelLeft(sliderPanel);
+
+        JLabel testingLabel = UIUtils.createLabel(row, "100% Testing  ");
+        testingLabel
+            .setToolTipText("To the testing activity, everything you do with Junit tests counts. Examples: writing, modifying, debugging, and executing Junit tests");
+
+        percentageProductionSlider = new JSlider(JSlider.HORIZONTAL);
+        percentageProductionSlider.setValue(this.productionPercentageStart);
+        percentageProductionSlider.setMinorTickSpacing(5);
+        percentageProductionSlider.setSnapToTicks(false);
+        percentageProductionSlider.setMaximum(100);
+        percentageProductionSlider.setMinimum(0);
+        percentageProductionSlider.setSize(questionPanel.getPreferredSize());
+        row.add(percentageProductionSlider);
+
+        JLabel productionLabel = UIUtils.createLabel(row, "  100% Production");
+        productionLabel
+            .setToolTipText("To the production activity, every activity that has to do with regular, non-test production code counts.");
+
+        JPanel sliderValuePanel = UIUtils.createFlowJPanelCenter(parent);
+        JLabel sliderValueText = UIUtils.createItalicLabel(
+            sliderValuePanel, "");
+        ChangeListener sliderMovedListener = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                try {
+                    Color backgroundColor = inputContainer.getBackground();
+                    colorAllDescendants(slider, backgroundColor);
+                } catch (Exception ex) {
+                }
+                int developmentTimeValue = percentageProductionSlider
+                    .getValue();
+                int testingTimeValue = 100 - developmentTimeValue;
+                sliderValueText.setText(testingTimeValue
+                    + "% Testing, " + developmentTimeValue
+                    + "% Production");
+                sliderValueText.updateUI();
+                sliderTouched = true;
+            }
+        };
+        percentageProductionSlider.addChangeListener(sliderMovedListener);
+        sliderMovedListener.stateChanged(null);
+        this.sliderTouched = false;
+
+        UIUtils.createLabel(
+            parent,
+            "Testing is every activity related to testing (reading, writing, modifying, refactoring and executing JUnit tests).");
+        UIUtils.createLabel(
+            parent,
+            "Production is every activity related to regular code (reading, writing, modifying, and refactoring Java classes).");
+    }
+
+    void colorAllDescendants(JComponent panel, Color color) {
+        panel.setBackground(color);
+
+        if (panel.getComponentCount() > 0)
+            for (Component component : panel.getComponents()) {
+                colorAllDescendants((JComponent) component, color);
+            }
+    }
+
     @Override
     boolean registerAction() {
+        if (!sliderTouched) {
+            JOptionPane.showMessageDialog(new JFrame(), "To proceed, you have to enter how you divide your time between production and test time, by at least touching the slider.", "Warning",
+                JOptionPane.WARNING_MESSAGE);
+            Color warningColor = new Color(255, 192, 178);
+            colorAllDescendants(slider, warningColor);
+
+            return false;
+        }
+
         Project project = new Project(Preferences.getInstance().getUserId());
 
         project.name = projectName.getText();
@@ -73,13 +174,15 @@ class ProjectRegistrationInputPanel extends RegistrationInputPanel {
         project.usesBugFindingSA = WizardStep.getChoiceFromButtonGroup(bugFindingUsage);
         project.usesOtherAutomationSA = WizardStep.getChoiceFromButtonGroup(automationUsage);
         project.usesToolsSA = toolsUsed.getText();
+        project.productionPercentage = this.percentageProductionSlider.getValue();
+        project.productionPercentageStart = this.productionPercentageStart;
 
         String projectId;
 
         try {
             projectId = new JsonTransferer().registerNewProject(project);
         } catch (Exception exception) {
-        	this.createFailureMessage(PROJECT_CREATION_MESSAGE_FAILURE, exception);
+            this.createFailureMessage(PROJECT_CREATION_MESSAGE_FAILURE, exception);
 
             return false;
         }
